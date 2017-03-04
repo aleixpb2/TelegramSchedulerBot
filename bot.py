2efe2509d7e46a8f2bc1814c54ff96bebebc4e20
jmigual <j.marce.igual@gmail.com>
2017-03-04 19:46:56 +0100

    Add bot basic scheme

---
+++ bot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import telebot
+
+TOKEN = '276486690:AAHVjZ369ib_Ms52vnEfY8s8D9Il0FxHyQA'
+bot = telebot.TeleBot(TOKEN)
+bot.get_me()
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    print(message.text)
+    bot.send_message(message.chat.id, "Hi! Welcome to the Telegram Scheduler Bot")
+
+
+@bot.message_handler(commands=['create_event'])
+def create_event(message):
+    bot.send_message(message.chat.id,
+                     "Fine! Now send me the starting time and the ending time in "
+                     "the following format:\n`dd/MM/yyyy HH:mm`",
+                     parse_mode="Markdown")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    
+    print("Message: ", message.text)
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
