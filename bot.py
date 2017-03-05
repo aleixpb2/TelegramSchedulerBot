0f5b1d5b634a5a4521ded925f6109b56ddd26985
Aleix París <aleixpb2@gmail.com>
2017-03-05 07:12:56 +0100

    get_data, send_data

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ from google_credentials import *
 
 # Commands
 # create_event - Create an event
-# delete_event - Delete an event
 # from - Starting time
 # to - Ending time
 # duration - Duration of the event
@@ -25,7 +24,7 @@ def printDebug(message):
 def myfun():
     return None
 
-def readDate(message, offset): # offset is to ignore commands /from and /to
+def read_date(message, offset): # offset is to ignore commands /from and /to
     # assume input is correct...
     day = int(message.text[offset:(offset+2)])
     month = int(message.text[(offset+3):(offset+5)])
@@ -42,6 +41,8 @@ def sendKeyboardButton(bot, id):
     markup.add(itembtna)
     bot.send_message(id, "Click to participate in this event:", reply_markup=markup)
 
+def get_data(id):
+    return data[id]
 
 TOKEN = '276486690:AAHVjZ369ib_Ms52vnEfY8s8D9Il0FxHyQA'
 bot = telebot.TeleBot(TOKEN)
@@ -97,11 +98,11 @@ def from_inp(message):
     printDebug(message)
     if states[message.chat.id] == stateEnum.create_event:
         states[message.chat.id] = stateEnum.from_create
-        d = readDate(message, len("/from ")) # print(d.strftime("%A"))
+        d = read_date(message, len("/from ")) # print(d.strftime("%A"))
         data[message.chat.id].append(d)
     elif states[message.chat.id] == stateEnum.delete_event:
         states[message.chat.id] = stateEnum.from_delete
-        d = readDate(message, len("/from "))
+        d = read_date(message, len("/from "))
         data[message.chat.id].append(d)
     else:
         bot.send_message(message.chat.id, "Unexpected command")
@@ -111,11 +112,11 @@ def to_inp(message):
     printDebug(message)
     if states[message.chat.id] == stateEnum.from_create:
         states[message.chat.id] = stateEnum.to_create
-        d = readDate(message, len("/to "))
+        d = read_date(message, len("/to "))
         data[message.chat.id].append(d)
     elif states[message.chat.id] == stateEnum.from_delete:
         states[message.chat.id] = stateEnum.to_delete
-        d = readDate(message, len("/to "))
+        d = read_date(message, len("/to "))
         data[message.chat.id].append(d)
     else:
         bot.send_message(message.chat.id, "Unexpected command")
@@ -127,8 +128,8 @@ def dur_inp(message):
         dur = int(message.text[len("/duration "):])
         print("Duration is " + str(dur))
         data[message.chat.id].append(dur)
+        data[message.chat.id].append(bot.get_chat_members_count(message.chat.id))
         print("Data: " + str(data[message.chat.id]))
-        ############# SEND data
         # Event created
         bot.send_message(message.chat.id, "Event created")
         #url = gc.get_credentials_url()
@@ -139,8 +140,8 @@ def dur_inp(message):
         dur = int(message.text[len("/duration "):])
         print("Duration is " + str(dur))
         data[message.chat.id].append(dur)
+        data[message.chat.id].append(bot.get_chat_members_count(message.chat.id))
         print("Data: " + str(data[message.chat.id]))
-        ############# SEND data
         # Delete event
         bot.send_message(message.chat.id, "Event deleted")
         states[message.chat.id] = stateEnum.none
@@ -157,27 +158,11 @@ def  test_callback(call):
     print("User: " + str(call.from_user) + " User id: " + str(call.from_user.id))
     if gc.has_credentials(str(call.from_user.id)):
         credentials = gc.get_credentials(str(call.from_user.id))
-        
+        send_credentials(credentials)
+
     else:
         bot.send_message(call.from_user.id, "Click this link to accept Google Calendar integration: " + gc.get_credentials_url())
 
-
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_inline(call):
-#     if call.inline_message_id:
-#         if call.data == "1":
-#             keyboard = types.InlineKeyboardMarkup(row_width=3)
-#             a = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             b = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             c = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             d = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             e = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             f = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             g = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             h = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             i = types.InlineKeyboardButton(text=" ", callback_data="2")
-#             keyboard.add(a, b, c, d, e, f, g, h, i)
-#             bot.edit_message_text(inline_message_id = call.inline_message_id, text = "X", reply_markup=keyboard)
 
 # Process webhook calls
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
@@ -210,18 +195,19 @@ if __name__ == "__main__":
     else:
         bot.polling()
 
-# Call example
-# {'message': {'left_chat_member': None, 'audio': None, 'new_chat_title': None, 'venue': None,
-#              'new_chat_photo': None, 'forward_from': None, 'migrate_from_chat_id': None,
-#              'content_type': 'text', 'caption': None, 'group_chat_created': None,
-#              'supergroup_chat_created': None, 'pinned_message': None,
-#              'from_user': <telebot.types.User object at 0x00000225D50422E8>, 'video': None,
-#              'voice': None, 'channel_chat_created': None, 'photo': None, 'date': 1488680389,
-#             'document': None, 'edit_date': None, 'message_id': 329, 'entities': None, 'new_chat_member': None,
-#             'sticker': None, 'reply_to_message': None, 'forward_date': None, 'delete_chat_photo': None,
-#             'chat': <telebot.types.Chat object at 0x00000225D504F6D8>, 'contact': None, 'migrate_to_chat_id': None,
-#             'forward_from_chat': None, 'text': 'Click to participate in this event:', 'location': None},
-#
-# 'data': 'Participate', 'id': '53080350164034740', 'from_user': {'username': 'Aleixpb2', 'id': 12358732,
-#                                                                 'first_name': 'Aleix', 'last_name': None},
-# 'game_short_name': None, 'chat_instance': '8816884457208723824', 'inline_message_id': None}
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_inline(call):
+#     if call.inline_message_id:
+#         if call.data == "1":
+#             keyboard = types.InlineKeyboardMarkup(row_width=3)
+#             a = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             b = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             c = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             d = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             e = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             f = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             g = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             h = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             i = types.InlineKeyboardButton(text=" ", callback_data="2")
+#             keyboard.add(a, b, c, d, e, f, g, h, i)
+#             bot.edit_message_text(inline_message_id = call.inline_message_id, text = "X", reply_markup=keyboard)
